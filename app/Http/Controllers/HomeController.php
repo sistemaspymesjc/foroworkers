b930a9e107fd168314a754f41d360a79d8f94e8a
jonathan <programadorjonathan@gmail.com>
2026-05-01 14:59:28 -0400

    sd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -116,13 +116,21 @@ class HomeController extends Controller
 
      // $ms_contents = $this->seoService->getModuleSeo($user->api_key_factory, $website, $forum->user_id, $forum->id);
 
-      $ms_contents = $this->moduleService->responseGet('/api/modules/seo',$user->api_key_factory, $website, $forum->user_id, $forum->id);
+     $ms_contents = $this->moduleService->responseGet('/api/modules/seo',$user->api_key_factory, $website, $forum->user_id, $forum->id);
 
      $m_seo = $ms_contents;
 
       // print_r($m_seo);
 
       // exit;
+
+     $ms_contentscol = $this->moduleService->responseGetPublic('/api/modules/getcol','posts');
+
+     $m_col = $ms_contentscol;
+
+      print_r($m_col);
+
+      exit;
 
 
 
@@ -150,7 +158,7 @@ class HomeController extends Controller
 
      // $mh_contents = $this->homeService->getModuleHome($user->api_key_factory, $website, $forum->user_id, $forum->id);
 
-      $mh_contents = $this->moduleService->responseGet('/api/modules/home',$user->api_key_factory, $website, $forum->user_id, $forum->id);
+     $mh_contents = $this->moduleService->responseGet('/api/modules/home',$user->api_key_factory, $website, $forum->user_id, $forum->id);
 
      // $m_seo = $ms_contents;
 
--- app/Http/Services/ModuleService.php
+++ app/Http/Services/ModuleService.php
@@ -33,6 +33,16 @@ class ModuleService implements ModuleInterface
 		return  $ms_contents = json_decode($response->getBody()->getContents());
 	}
 
+	public function responseGetPublic($segment,$table_name)
+	{        
+
+		$endpoint = env('APP_ENDPOINT_FACTORY').$table_name;        
+
+		$response = $this->clients->request('GET', $endpoint);      
+
+		return  $ms_contents = json_decode($response->getBody()->getContents());
+	}
+
 	public function responsePost($segment,$api_key_factory,$website,$user_id,$software_id)
 	{      
 
